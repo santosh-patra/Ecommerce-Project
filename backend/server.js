78b5bf2baaba953204f9ee72b07d8592d7836884
santosh-patra <patrasantoshkl@gmail.com>
2022-01-17 15:14:50 +0530

    added error controller in backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv')
 const connectDatabase = require('./config/database')
 
 
+// handling uncaught exception'
+process.on("uncaughtException",(err)=>{
+    console.log(`Error:${err.message}`)
+    console.log(`shutting down the server due to uncaught exception`);
+    process.exit(1);
+})
 
 // config
 dotenv.config({path:"backend/config/config.env"})
@@ -10,6 +16,16 @@ dotenv.config({path:"backend/config/config.env"})
 // DB Connection
 connectDatabase()
 
-app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
     console.log(`Server running on port : ${process.env.PORT}`)
+})
+
+// unhandled Promise rejecetion
+process.on("unhandeledRejection",err=>{
+    console.log(`Error:${err.message}`);
+    console.log(`Shutting down the error due to unhandled promise rejection`)
+
+    server.close(()=>{
+        process.exit(1);
+    })
 })
