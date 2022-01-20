a706ad12bf6c462afa830454f48eb8d88bcfaa7c
santosh-patra <patrasantoshkl@gmail.com>
2022-01-20 20:31:51 +0530

    changes in app.js , model and controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -13,9 +13,36 @@ exports.registerUser = catchAsyncError(async(req,res,next)=>{
             url:"Sample URL"
         }
     })
+    const token = user.getJWTToken()
     res.status(201).json({
         success:true,
         Message:"A new User Added",
+        Token:token,
         details:user
     })
 }) 
+
+// login user
+exports.loginUser = catchAsyncError(async(req,res,next)=>{
+    const {email,password} = req.body;
+
+    if (!email || !password){
+        return next(new ErrorHandler("Please enter Email or password",400))
+    }
+    const user = User.findOne({email}).select("+password")
+
+    if(!user){
+        return next(new ErrorHandler("Invalid email or password",401))
+    }
+    const isPasswordMatched = user.comparePassword()
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid email or password",401))
+    }
+    const token = user.getJWTToken()
+    res.status(200).json({
+        success:true,
+        Token:token,
+        details:user
+    })
+})
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
  
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,6 +39,22 @@ const userSchema = new mongoose.Schema({
   resetPasswordToken:String,
   resetPasswordExpire: Date
 }); 
+
+userSchema.pre("save",async function(next){
+
+  if (!this.isModified("password")){
+    next()
+  }
+
+  this.password = await bcrypt.hash(this.password,10)
+})
+
+// JWT token
+userSchema.methods.getJWTToken = function(){
+  return jwt.sign({id:this._id},process.env.JWT_SECRET,{
+    expiresIn:process.env.JWT_EXPIRE,
+  })
+}
 
 
 
